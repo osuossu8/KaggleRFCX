dfd59a364eb40635dddd1c5f4b5d2bc6c5b979aa
osuossu8 <osuosuossu18@gmail.com>
2021-02-08 12:16:12 +0000

    040

--- src/datasets2.py
+++ src/datasets2.py
@@ -39,11 +39,8 @@ def crop_or_pad(y, sr, period, record, mode="train"):
 
 
 class SedDatasetV6:
-    def __init__(self, df, period=10, stride=5, 
-                 audio_transform=None, 
-                 wave_form_mix_up_ratio=None,
-                 data_path="train", mode="train"):
-
+    def __init__(self, df, period=10, stride=5, audio_transform=None, 
+                 wave_form_mix_up_ratio=None, data_path="train", mode="train"):
         self.period = period
         self.stride = stride
         self.audio_transform = audio_transform
@@ -104,3 +101,115 @@ class SedDatasetV6:
         }
 
 
+class SedDatasetV7:
+    def __init__(self, df, period=10, stride=5, audio_transform=None,
+                 wave_form_mix_up_ratio=None, data_path="train", mode="train"):
+        self.period = period
+        self.stride = stride
+        self.audio_transform = audio_transform
+        self.wave_form_mix_up_ratio = wave_form_mix_up_ratio
+        self.data_path = data_path
+        self.mode = mode
+        self.df = df.groupby("recording_id").agg(lambda x: list(x)).reset_index()
+        self.len_df = len(self.df)
+
+    def __len__(self):
+        return self.len_df
+
+    def __getitem__(self, idx):
+        record = self.df.iloc[idx]
+        y, sr = sf.read(f"{self.data_path}/{record['recording_id']}.flac")
+        y, label = crop_or_pad(y, sr, period=self.period, record=record, mode=self.mode)
+
+        if self.audio_transform:
+            y = self.audio_transform(samples=y, sample_rate=sr)
+
+        if self.wave_form_mix_up_ratio:
+            r = np.random.random()
+            r2 = np.random.random()
+            # do mixup
+            l = [i for i in range(self.len_df)]
+            l.pop(idx)
+            rand_idx = np.random.choice(l)
+
+            record2 = self.df.iloc[rand_idx]
+            y2, sr2 = sf.read(f"{self.data_path}/{record2['recording_id']}.flac")
+            y2, label2 = crop_or_pad(y2, sr2, period=self.period, record=record2, mode=self.mode)
+
+            if r <= 0.4:
+                y = normalize(y.reshape(-1, 1)) * self.wave_form_mix_up_ratio + normalize(y2.reshape(-1, 1)) * (1- self.wave_form_mix_up_ratio)
+                y = np.squeeze(y)
+
+                if r2 > 0.6:
+                    label = label * self.wave_form_mix_up_ratio + label2 * ( 1- self.wave_form_mix_up_ratio)
+                else:
+                    label = label + label2
+                    label = np.where(label > 0, 1.0, 0).astype(np.float32)
+
+            elif r > 0.4 and r <= 0.7:
+                if r2 > 0.5:
+                    y = np.concatenate([y[:int(sr*self.wave_form_mix_up_ratio)], y2[int(sr*self.wave_form_mix_up_ratio):]]).astype(np.float32)
+                else:
+                    y = np.concatenate([y2[:int(sr*self.wave_form_mix_up_ratio)], y[int(sr*self.wave_form_mix_up_ratio):]]).astype(np.float32)
+                label = label + label2
+                label = np.where(label > 0, 1.0, 0).astype(np.float32)
+            else:
+                pass
+
+        return {
+            "image" : y,
+            "target" : label,
+            "id" : record['recording_id']
+        }
+
+
+class SedDatasetV8:
+    def __init__(self, df, period=10, stride=5, audio_transform=None,
+                 wave_form_mix_up_ratio=None, data_path="train", mode="train"):
+        self.period = period
+        self.stride = stride
+        self.audio_transform = audio_transform
+        self.wave_form_mix_up_ratio = wave_form_mix_up_ratio
+        self.data_path = data_path
+        self.mode = mode
+        self.df = df.groupby("recording_id").agg(lambda x: list(x)).reset_index()
+        self.len_df = len(self.df)
+
+    def __len__(self):
+        return self.len_df
+
+    def __getitem__(self, idx):
+        record = self.df.iloc[idx]
+        y, sr = sf.read(f"{self.data_path}/{record['recording_id']}.flac")
+        y, label = crop_or_pad(y, sr, period=self.period, record=record, mode=self.mode)
+
+        if self.audio_transform:
+            y = self.audio_transform(samples=y, sample_rate=sr)
+
+        if self.wave_form_mix_up_ratio:
+            r = np.random.random()
+            r2 = np.random.random()
+            # do mixup
+            l = [i for i in range(self.len_df)]
+            l.pop(idx)
+            rand_idx = np.random.choice(l)
+
+            record2 = self.df.iloc[rand_idx]
+            y2, sr2 = sf.read(f"{self.data_path}/{record2['recording_id']}.flac")
+            y2, label2 = crop_or_pad(y2, sr2, period=self.period, record=record2, mode=self.mode)
+
+            if r > 0.5:
+                if r2 > 0.5:
+                    y = np.concatenate([y[:int(sr*self.wave_form_mix_up_ratio)], y2[int(sr*self.wave_form_mix_up_ratio):]]).astype(np.float32)
+                else:
+                    y = np.concatenate([y2[:int(sr*self.wave_form_mix_up_ratio)], y[int(sr*self.wave_form_mix_up_ratio):]]).astype(np.float32)
+                label = label + label2
+                label = np.where(label > 0, 1.0, 0).astype(np.float32)
+            else:
+                pass
+
+        return {
+            "image" : y,
+            "target" : label,
+            "id" : record['recording_id']
+        }
