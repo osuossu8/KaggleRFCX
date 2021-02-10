4ed8d68ccf98a562e8796dd235f295d4aa55bdea
osuossu8 <osuosuossu18@gmail.com>
2021-02-10 03:11:38 +0000

    add f1 score

--- src/engine.py
+++ src/engine.py
@@ -63,6 +63,41 @@ def _lwlrap_sklearn(truth, scores):
     return overall_lwlrap
 
 
+def f1_loss(y_true, y_pred, is_training=False) -> torch.Tensor:
+    '''Calculate F1 score. Can work with gpu tensors
+    
+    The original implmentation is written by Michal Haltuf on Kaggle.
+    
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. 0 <= val <= 1
+    
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+    
+    '''
+    
+    y_pred = y_pred > 0.5
+    
+    tp = (y_true * y_pred).sum()#.to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred))#.sum().to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum()#.to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum()#.to(torch.float32)
+    
+    epsilon = 1e-7
+    
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+    
+    f1 = 2* (precision*recall) / (precision + recall + epsilon)
+    #f1.requires_grad = is_training #pytorchで利用する際は自動微分が可能 requires_gradで
+    return f1
+
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
 
@@ -99,8 +134,10 @@ class MetricMeter(object):
         score_class, weight = lwlrap(np.array(self.y_true), np.array(self.y_pred))
         self.score = (score_class * weight).sum()
         # self.score = _lwlrap_sklearn(np.array(self.y_true), np.array(self.y_pred)) #(score_class * weight).sum()
+        self.f1_score = f1_loss(np.array(self.y_true), np.array(self.y_pred))
         return {
-            "lwlrap" : self.score
+            "lwlrap" : self.score,
+            "f1_score": self.f1_score
         }
 
 
